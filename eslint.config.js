e5bd2ba81ac87c6bdfa142321142486460bc6b9a
Stanislav Mishchyshyn <stanislav.mishchyshyn@namecheap.com>
2024-10-22 19:20:08 +0300

    chore: update devDependencies

---
+++ eslint.config.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const babelParser = require('@babel/eslint-parser');
+const prettier = require('eslint-plugin-prettier');
+const globals = require('globals');
+
+/**
+ * @type {import('eslint').Linter.Config}
+ */
+module.exports = [
+    {
+        ignores: [
+            'src/pipe.min.js',
+            '**/coverage',
+            'examples/fragment-performance/hooks.js',
+            'examples/fragment-performance/test.js'
+        ]
+    },
+    {
+        plugins: {
+            prettier
+        },
+
+        languageOptions: {
+            globals: {
+                ...Object.fromEntries(
+                    Object.entries(globals.browser).map(([key]) => [key, 'off'])
+                ),
+                ...globals.node,
+                require: false,
+                module: false
+            },
+
+            parser: babelParser,
+            ecmaVersion: 5,
+            sourceType: 'script',
+
+            parserOptions: {
+                requireConfigFile: false
+            }
+        },
+
+        rules: {
+            curly: 2,
+            'no-underscore-dangle': 0,
+            'no-constant-condition': 2,
+            'no-dupe-args': 2,
+            'no-debugger': 2,
+            'no-duplicate-case': 2,
+            'no-empty': 2,
+            'no-empty-character-class': 2,
+            'no-eval': 2,
+            'no-unused-vars': 2,
+            'no-lonely-if': 2,
+            quotes: [0, true, 'single'],
+            strict: [2, 'global'],
+
+            'prettier/prettier': [
+                2,
+                {
+                    singleQuote: true,
+                    tabWidth: 4
+                }
+            ]
+        }
+    }
+];
